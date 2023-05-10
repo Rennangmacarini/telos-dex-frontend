e1596754c6e78ef2f2eb03e0a3c4a7c2c309e328
Rennan Macarini <rennangmacarini@gmail.com>
2023-05-10 00:18:47 -0300

    feat: `:art: ` adjusting the axois header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,8 @@
 
 import { HeaderMenu} from "../../styles/styles"
 
-
-
-
 export function Header(){
   return(
-
     <HeaderMenu>
        <img src="/img/logo.png" alt="" />
        <h1>Pokédex</h1>
