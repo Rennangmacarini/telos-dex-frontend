a9b5820371391462f91d592f39a9f96e9dd8925d
Rennan Macarini <rennangmacarini@gmail.com>
2023-05-11 01:07:58 -0300

    feat: `:zap: ` updating index

--- src/components/CardPokemons/index.jsx
+++ src/components/CardPokemons/index.jsx
@@ -1,21 +1,20 @@
-import { Card, Ground, Content  } from "../../styles/styles";
-import { PokedexContext } from "../../contexts/PokedexContext";
-import { useContext } from "react";
+import { Card, Ground, Content } from "../../styles/styles";
+
 import { Link } from "react-router-dom";
 
-export function CardPokemons(){
-  const [pokemons, setPokemons] = useContext(PokedexContext)
-
-  return(
+export function CardPokemons({ pokemons }) {
+  return (
     <Content>
-    {pokemons.map((pokemon, key) =>
-      <Card key={key}>
-        <h1>#{pokemon.game_indices[0].game_index}</h1>
-       <Link to="/Information"><img src={pokemon.sprites.other.home.front_default} alt="" /></Link>
-        <h2>{pokemon.name}</h2>
-        <Ground />
-      </Card>
-    )}
-  </Content>
-  )
+      {pokemons?.map((pokemon, key) => (
+        <Card key={key}>
+          <h1>#{pokemon.id}</h1>
+          <Link to={`/Information/${pokemon.id}`}>
+            <img src={pokemon.sprites.other.home.front_default} alt="" />
+          </Link>
+          <h2>{pokemon.name}</h2>
+          <Ground />
+        </Card>
+      ))}
+    </Content>
+  );
 }
