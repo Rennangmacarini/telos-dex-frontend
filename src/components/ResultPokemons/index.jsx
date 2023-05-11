93fb6555fa2358612ef93020f8c5cb7c21a681c8
Rennan Macarini <rennangmacarini@gmail.com>
2023-05-11 01:09:23 -0300

    feat: `:fire:` changing index and styles

--- src/components/ResultPokemons/index.jsx
+++ src/components/ResultPokemons/index.jsx
@@ -1,87 +1,283 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PokedexContext } from "../../contexts/PokedexContext";
-import { Contents, ContentCards, CardPokemon } from "./styles";
-import { FiArrowLeft } from 'react-icons/fi'
+import { Contents, ContentCards, CardPokemon, ContenainerCards } from "./styles";
+import { FiArrowLeft } from "react-icons/fi";
 import { TbWeight } from "react-icons/tb";
 import { CiLineHeight } from "react-icons/ci";
-import { LinearProgress } from "@mui/material";
-import { Link } from "react-router-dom";
+import { CircularProgress, LinearProgress } from "@mui/material";
+import { Link, useParams } from "react-router-dom";
+import { useMemo } from "react";
 
 export function ResultPokemons() {
-  const [pokemons, setPokemons] = useContext(PokedexContext)
+  const [pokemons, setPokemons, search, loading] = useContext(PokedexContext);
+  const [pokemon, setSelectedPokemon] = useState({});
+  const { pokemonId } = useParams();
 
+
+  useMemo(() => {
+    if (loading) {
+      return;
+    }
+    if (pokemons.length > 0) {
+      setSelectedPokemon(
+        pokemons.filter((pokemon) => pokemon.id.toString() === pokemonId.toString())[0]
+      );
+    } else {
+      search();
+    }
+  }, [pokemonId, pokemons]);
+
+  const colorsList = [
+    { type: `grass`, color: `#74CB48` },
+    { type: `fire`, color: `#F57D31` },
+    { type: `bug`, color: `#A7B723` },
+    { type: `dark`, color: `#75574C` },
+    { type: `dragon`, color: `#7037FF` },
+    { type: `fairy`, color: `#E69EAC` },
+    { type: `fighting`, color: `#C12239` },
+    { type: `flying`, color: `#A891EC` },
+    { type: `ghost`, color: `#70559B` },
+    { type: `normal`, color: `#AAA67F` },
+    { type: `ground`, color: `#DEC16B` },
+    { type: `ice`, color: `#9AD6DF` },
+    { type: `psychic`, color: `#FB5584` },
+    { type: `rock`, color: `#B69E31` },
+    { type: `water`, color: `#6493EB` },
+  ];
 
   return (
-    <Contents>
-      {pokemons.map((pokemon, key) =>
-        <div key={key}>
-          <div style={{backgroundColor: "#A6A6A6", padding: "0.5rem 1rem" ,borderRadius: "8px", color: "white", fontWeight: "bold"}}>
-            <ContentCards >
-              <CardPokemon>
-               <Link to="/"><FiArrowLeft style={{fontSize: "25px"}} /></Link>
-                <h2>{pokemon.name}</h2>
-              </CardPokemon>
-              <h3>#{pokemon.game_indices[0].game_index}</h3>
-            </ContentCards>
-            <img src={pokemon.sprites.other.home.front_default} alt="" />
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "24px" }}>
-              <h4>{pokemon.types[0].type.name}</h4>
-              <h4>{pokemon.types[1].type.name}</h4>
-            </div>
-            <h1 style={{ margin: "1rem 0rem" }}>About</h1>
-          </div>
-          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "25px" }}>
-            <div style={{ display: "flex", alignItems: "center", flexDirection: "column", gap: "5px", borderRight: "1px solid #E0E0E0", paddingRight: "25px" }}>
-              <div style={{ display: "flex", alignItems: "center", gap: "5px" }}>
-                <TbWeight />
-                <h4>{pokemon.weight}</h4>
+
+    <>
+
+      {loading && <CircularProgress />}
+      {pokemon?.id && (
+        <Contents style={{
+          position: "relative",
+          zIndex: "2",
+          backgroundColor: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+            `black`
+            }`
+        }}>
+          <ContenainerCards/>
+            <div key={pokemon.id}>
+              <div
+                className={`color-${pokemon.types[0].type.name}`}
+                style={{
+                  padding: "0.5rem 1rem",
+                  borderRadius: "8px",
+                  color: "white",
+                  fontWeight: "bold",
+                }}
+              >
+                <ContentCards>
+                  <CardPokemon>
+                    <Link to="/">
+                      <FiArrowLeft style={{ fontSize: "25px" }} />
+                    </Link>
+                    <h2>{pokemon.name}</h2>
+                  </CardPokemon>
+
+                  <h3>#{pokemon.game_indices[0].game_index}</h3>
+                </ContentCards>
+                <img src={pokemon.sprites.other.home.front_default} alt="" />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "24px",
+                  }}
+                >
+                  {pokemon.types.map((type) => {
+                    return (
+                      <h4
+                        style={{
+                          padding: "2px 10px",
+                          borderRadius: "10px",
+                          fontSize: "12px",
+                          backgroundColor: `${colorsList.filter((color) => color.type === type.type.name)[0]?.color ??
+                            `#A43E9E`
+                            }`,
+                        }}
+                      >
+                        {type.type.name}
+                      </h4>
+                    );
+                  })}
+                </div>
+                <h1 style={{ margin: "16px 0px", color: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+            `black`
+            }`}}>About</h1>
               </div>
-              <h5>Weight</h5>
-            </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "25px",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    gap: "5px",
+                    borderRight: "1px solid #E0E0E0",
+                    paddingRight: "25px",
+                    marginLeft: "-10px"
+                  }}
+                >
+                  <div style={{ display: "flex", alignItems: "center", gap: "5px" }}>
+                    <TbWeight />
+                    <h4>{pokemon.weight}</h4>
+                  </div>
+                  <h5>Weight</h5>
+                </div>
 
-            <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
-              <div style={{ display: "flex", alignItems: "center", gap: "5px" }}>
-                <CiLineHeight />
-                <h4>{pokemon.height}</h4>
+                <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+                  <div style={{ display: "flex", alignItems: "center", gap: "5px" }}>
+                    <CiLineHeight />
+                    <h4>{pokemon.height}</h4>
+                  </div>
+                  <h5>Height</h5>
+                </div>
               </div>
-              <h5>Height</h5>
-            </div>
+
+              <h1 style={{ margin: "1rem 0rem",  color: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+            `black`
+            }`  }}>Base Stats</h1>
+              <div style={{ display: "flex", alignItems: "center", gap: "25px" }}>
+                <div style={{ borderRight: "1px solid #E0E0E0", paddingRight: "15px" }}>
+                  <h5 style={{color: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+            `black`
+            }`}}>HP</h5>
+                  <h5
+                  style={{color: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                    `black`
+                    }`}}
+                  >ATK</h5>
+                  <h5
+                  style={{color: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                    `black`
+                    }`}}
+                  >DEF</h5>
+                  <h5
+                  style={{color: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                    `black`
+                    }`}}
+                  >SATK </h5>
+                  <h5
+                  style={{color: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                    `black`
+                    }`}}
+                  
+                  >SDEF</h5>
+                  <h5
+                  style={{color: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                    `black`
+                    }`}}
+                  
+                  >SPD</h5>
+                </div>
+
+                <div>
+                  <h5>{pokemon.stats[0].base_stat}</h5>
+                  <h5>{pokemon.stats[1].base_stat}</h5>
+                  <h5>{pokemon.stats[2].base_stat}</h5>
+                  <h5>{pokemon.stats[3].base_stat}</h5>
+                  <h5>{pokemon.stats[4].base_stat}</h5>
+                  <h5>{pokemon.stats[5].base_stat}</h5>
+                </div>
+
+                <div>
+                  <h6>
+                    <LinearProgress
+                      sx={{
+                        '& .MuiLinearProgress-bar': {backgroundColor: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                          `black`
+                          }`}
+                      }}
+                      style={{ height: "6px", borderRadius: "4px", opacity: "0.4", color: `black` }}
+                      variant="determinate"
+                      value={pokemon.stats[0].base_stat}
+                    />
+                  </h6>
+                  <h6>
+                    <LinearProgress
+                    sx={{
+                      '& .MuiLinearProgress-bar': {backgroundColor: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                        `black`
+                        }`}
+                    }}
+
+                      color="inherit"
+                      style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }}
+                      variant="determinate"
+                      value={pokemon.stats[1].base_stat}
+                    />
+                  </h6>
+                  <h6>
+                    <LinearProgress
+                    sx={{
+                      '& .MuiLinearProgress-bar': {backgroundColor: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                        `black`
+                        }`}
+                    }}
+
+                      color="inherit"
+                      style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }}
+                      variant="determinate"
+                      value={pokemon.stats[2].base_stat}
+                    />
+                  </h6>
+                  <h6>
+                    <LinearProgress
+                    sx={{
+                      '& .MuiLinearProgress-bar': {backgroundColor: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                        `black`
+                        }`}
+                    }}
+
+                      color="inherit"
+                      style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }}
+                      variant="determinate"
+                      value={pokemon.stats[3].base_stat}
+                    />
+                  </h6>
+                  <h6>
+                    <LinearProgress
+                    sx={{
+                      '& .MuiLinearProgress-bar': {backgroundColor: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                        `black`
+                        }`}
+                    }}
+
+                      color="inherit"
+                      style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }}
+                      variant="determinate"
+                      value={pokemon.stats[4].base_stat}
+                    />
+                  </h6>
+                  <h6>
+                    <LinearProgress
+                    sx={{
+                      '& .MuiLinearProgress-bar': {backgroundColor: `${colorsList.filter((color) => color.type === pokemon.types[0].type.name)[0]?.color ??
+                        `black`
+                        }`}
+                    }}
+                      color="inherit"
+                      style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }}
+                      variant="determinate"
+                      value={pokemon.stats[5].base_stat}
+                    />
+                  </h6>
+                </div>
+              </div>
           </div>
+        </Contents>
+      )}
 
-          <h1 style={{ margin: "1rem 0rem" }}>Base Stats</h1>
-          <div style={{ display: "flex", alignItems: "center", gap: "25px" }}>
-
-            <div style={{ borderRight: "1px solid #E0E0E0", paddingRight: "15px" }}>
-              <h5>HP</h5>
-              <h5>ATK</h5>
-              <h5>DEF</h5>
-              <h5>SATK </h5>
-              <h5>SDEF</h5>
-              <h5>SPD</h5>
-            </div>
-
-            <div>
-              <h5>{pokemon.stats[0].base_stat}</h5>
-              <h5>{pokemon.stats[1].base_stat}</h5>
-              <h5>{pokemon.stats[2].base_stat}</h5>
-              <h5>{pokemon.stats[3].base_stat}</h5>
-              <h5>{pokemon.stats[4].base_stat}</h5>
-              <h5>{pokemon.stats[5].base_stat}</h5>
-            </div>
-
-            <div>
-              <h6><LinearProgress color="inherit" style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }} variant="determinate" value={pokemon.stats[0].base_stat} /></h6>
-              <h6><LinearProgress color="inherit" style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }} variant="determinate" value={pokemon.stats[1].base_stat} /></h6>
-              <h6><LinearProgress color="inherit" style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }} variant="determinate" value={pokemon.stats[2].base_stat} /></h6>
-              <h6><LinearProgress color="inherit" style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }} variant="determinate" value={pokemon.stats[3].base_stat} /></h6>
-              <h6><LinearProgress color="inherit" style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }} variant="determinate" value={pokemon.stats[4].base_stat} /></h6>
-              <h6><LinearProgress color="inherit" style={{ height: "6px", borderRadius: "4px", opacity: "0.4" }} variant="determinate" value={pokemon.stats[5].base_stat} /></h6>
-            </div>
-          </div>
-        </div>
-      )
-      }
-
-    </Contents >
-  )
+    </>
+  );
 }
--- src/components/ResultPokemons/styles.js
+++ src/components/ResultPokemons/styles.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const Contents = styled.div`
   width: 350px;
-  
-  background-color: ${({ theme }) => theme.COLORS.WHITE};
   box-shadow: inset 0px 1px 3px 1px rgba(0, 0, 0, 0.25);
   border-radius: 8px;
   margin-top: 24px;
@@ -12,7 +10,6 @@ export const Contents = styled.div`
   color: #383838;
 
   img {
-    
     width: 200px;
   }
 
@@ -22,7 +19,7 @@ export const Contents = styled.div`
 
   h5 {
     margin: 5px 0px;
-    color: #1D1D1D;
+    color: #1d1d1d;
   }
 
   h6 {
@@ -32,28 +29,38 @@ export const Contents = styled.div`
     margin: 12px 0px;
     border-radius: 4px;
   }
+`;
 
+export const ContenainerCards = styled.div`
+  background-color: white;
+  width: 320px;
+  padding: 1rem;
+  height: 380px;
+  position: absolute;
+  z-index: -1;
+  bottom: 10px;
+  margin-left: -10px;
+  box-shadow: inset 0px 1px 3px 1px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
 `;
 
 export const ContentCards = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const CardPokemon = styled.div`
-display: flex;
-gap: 15px;
-align-items: center;
+  display: flex;
+  gap: 15px;
+  align-items: center;
 
-a {
-  color: white;
-  margin-top: 0.2rem;
-}
+  a {
+    color: white;
+    margin-top: 0.2rem;
+  }
 
-a:hover{
-  color: #383838;
-}
-
-`
+  a:hover {
+    color: #383838;
+  }
+`;
